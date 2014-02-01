79327330b42f52f76e0b9ccf8cda6db2a1234a03
Ubuntu <ubuntu@ip-10-239-27-253.ec2.internal>
2014-02-01 02:15:10 +0000

    test

--- js/mapmarker_func.jquery.js
+++ js/mapmarker_func.jquery.js
@@ -2,15 +2,27 @@ $(document).ready(function(){
 	
 		//set up markers 
 		var myMarkers = {"markers": [
-				{"latitude": "34.095085", "longitude":"-118.281671", "icon": "img/map-marker.png", "baloon_text": '<strong>PLANAR</strong> 1110 Bates Avenue Los Angeles, CA 90029, US'}
+				{"latitude": "41.4169", "longitude":"-73.3997", "icon": "img/map-marker.png", "baloon_text": '<strong>Sportsplex</strong> 13 Stony Hill Rd, Bethel CT 06801'}
 			]
 		};
 		
+		var myMarkers2 = {"markers": [
+				{"latitude": "41.3912", "longitude":"-73.2882", "icon": "img/map-marker.png", "baloon_text": '<strong>Sportsplex</strong> 13 Stony Hill Rd, Bethel CT 06801'}
+			]
+		};
 		//set up map options
 		$("#map").mapmarker({
-			zoom	: 11,
-			center	: '1110 Bates Avenue Los Angeles, CA 90029, United States',
+			zoom	: 14,
+			center	: '13 Stony Hill Rd, Bethel CT 06801',
 			markers	: myMarkers
+		});
+		
+		//set up map options
+		$("#map.second.map").mapmarker({
+			zoom	: 14,
+			center	: '3 Simm Lane, Newtown, CT',
+			markers	: myMarkers2
 		});
 
 	});
+	
