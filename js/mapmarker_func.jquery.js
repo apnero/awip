ce207a089be4d0e7a0d775d60e9dceb315d4cb3b
andrew.nero@gmail.com <ubuntu@ip-10-239-27-253.ec2.internal>
2014-02-14 00:42:28 +0000

    some updates

--- js/mapmarker_func.jquery.js
+++ js/mapmarker_func.jquery.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 	
 		//set up markers 
 		var myMarkers = {"markers": [
-				{"latitude": "41.4169", "longitude":"-73.3997", "icon": "img/map-marker.png", "baloon_text": '<strong>Sportsplex</strong> 13 Stony Hill Rd, Bethel CT 06801'}
+				{"latitude": "41.4169", "longitude":"-73.3997", "icon": "img/map-marker.png", "baloon_text": '<strong>Sportsplex</strong> 13 Stony Hill Rd, Bethel, CT'}
 			]
 		};
 		
 		var myMarkers2 = {"markers": [
-				{"latitude": "41.3912", "longitude":"-73.2882", "icon": "img/map-marker.png", "baloon_text": '<strong>Sportsplex</strong> 13 Stony Hill Rd, Bethel CT 06801'}
+				{"latitude": "41.3912", "longitude":"-73.2882", "icon": "img/map-marker.png", "baloon_text": '<strong>Cannon Ridge</strong> 3 Simm Lane, Newtown, CT'}
 			]
 		};
 		//set up map options
