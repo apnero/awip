ea0035a7ae7454c7f19d14d981e64ea1ec8789fc
andrew.nero@gmail.com <ubuntu@ip-10-238-220-210.ec2.internal>
2014-04-01 03:15:23 +0000

    latest

--- js/mapmarker_func.jquery.js
+++ js/mapmarker_func.jquery.js
@@ -2,14 +2,20 @@ $(document).ready(function(){
 	
 		//set up markers 
 		var myMarkers = {"markers": [
-				{"latitude": "41.4169", "longitude":"-73.3997", "icon": "img/map-marker.png", "baloon_text": '<strong>Sportsplex</strong> 13 Stony Hill Rd, Bethel, CT'}
+				{"latitude": "41.4169", "longitude":"-73.3997", "icon": "img/map-marker.png", "baloon_text": '<strong><a href="http://www.sportsplexbethel.com" target="_blank">Sportsplex</a></strong> 13 Stony Hill Rd, Bethel, CT'}
 			]
 		};
 		
 		var myMarkers2 = {"markers": [
-				{"latitude": "41.3912", "longitude":"-73.2882", "icon": "img/map-marker.png", "baloon_text": '<strong>Cannon Ridge</strong> 3 Simm Lane, Newtown, CT'}
+				{"latitude": "41.441583", "longitude":"-73.404036", "icon": "img/map-marker.png", "baloon_text": '<strong><a href="http://www.anytimefitness.com/gyms/169/brookfield-ct-06804" target="_blank">Anytime Fitness in Brookfield</a></strong> 195 Federal Rd, Brookfield, CT'}
 			]
 		};
+		
+		var myMarkers3 = {"markers": [
+				{"latitude": "41.129518", "longitude":"-73.385458", "icon": "img/map-marker.png", "baloon_text": '<strong><a href="http://www.achievefit.net/" target="_blank">Achieve Rehab and Fitness in Norwalk</a></strong> 27 Lois St, Norwalk, CT'}
+			]
+		};
+		
 		//set up map options
 		$("#map").mapmarker({
 			zoom	: 14,
@@ -20,8 +26,15 @@ $(document).ready(function(){
 		//set up map options
 		$("#map.second.map").mapmarker({
 			zoom	: 14,
-			center	: '3 Simm Lane, Newtown, CT',
+			center	: '195 Federal Rd, Brookfield, CT',
 			markers	: myMarkers2
+		});
+
+		//set up map options
+		$("#map.third.map").mapmarker({
+			zoom	: 14,
+			center	: '27 Lois St, Norwalk, CT',
+			markers	: myMarkers3
 		});
 
 	});
